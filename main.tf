a9a9f0ef82c56f167e615c91d28d05ac2a1e0752
kaiwen720 <62673045+kaiwen720@users.noreply.github.com>
2020-11-13 10:30:26 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,16 +59,13 @@ resource "aws_security_group" "default" {
   name        = "terraform_example"
   description = "Used in the terraform"
   vpc_id      = aws_vpc.default.id
-  ingress {
-    cidr_blocks = ["10.0.0.0/24"]
-  }
 
   # SSH access from anywhere
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 
   # HTTP access from the VPC
