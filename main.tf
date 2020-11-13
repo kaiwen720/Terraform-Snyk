3f822337711c43b2d35c4bb7834c815ab08ec5d0
kaiwen720 <62673045+kaiwen720@users.noreply.github.com>
2020-11-13 09:29:30 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "elb" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 
   # outbound internet access
@@ -49,7 +49,7 @@ resource "aws_security_group" "elb" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 }
 
@@ -59,6 +59,7 @@ resource "aws_security_group" "default" {
   name        = "terraform_example"
   description = "Used in the terraform"
   vpc_id      = aws_vpc.default.id
+  cidr_blocks = ["10.0.0.0/24"]
 
   # SSH access from anywhere
   ingress {
@@ -81,12 +82,13 @@ resource "aws_security_group" "default" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 }
 
 resource "aws_elb" "web" {
   name = "terraform-example-elb"
+  internal = true
 
   subnets         = [aws_subnet.default.id]
   security_groups = [aws_security_group.elb.id]
@@ -108,6 +110,7 @@ resource "aws_key_pair" "auth" {
 resource "aws_instance" "web" {
   # The connection block tells our provisioner how to
   # communicate with the resource (instance)
+  root_block_device.encrypted = true
   connection {
     type = "ssh"
     # The default username for our AMI
